f9d8fda5d2606568cc80d4a0fc78397acd054b11
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2018-11-25 17:31:37 +0530

    Changed index view

--- report/views.py
+++ report/views.py
@@ -4,17 +4,20 @@ import requests
 from django.http import FileResponse, HttpResponseRedirect
 from . forms import Form
 
-def func(request):
-    if request.method=="POST":
-        id=request.POST.get("id")
-        ans = query.objects.get(id=id)
-        response=ans.repo
-        if ans is None:
-            return redirect("index.html")
-        else:
-            #return render (request,"ans.html",{'ans':response})
-            return redirect(response)
-            
+
 def index(request):
-    form = Form
+    if request.method == "POST":
+        form = Form(request.POST)
+        if form.is_valid():
+            id=request.POST.get("id")
+            ans = query.objects.get(id=id)
+            response=ans.repo
+            if ans is None:
+                return redirect("index.html")
+            else:
+                #return render (request,"ans.html",{'ans':response})
+                return redirect(response)
+    else:
+        form = Form()
     return render(request,"index.html",{'form':form})
+
