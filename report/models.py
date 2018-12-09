59ca72fe7f3e6fc2c2be366bee0b5653cb09b1c1
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2018-12-09 15:40:02 +0530

    Changes in model

--- report/models.py
+++ report/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class query(models.Model):
     id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30, null=True, blank = True)
+    repo=models.FileField(upload_to='documents/',null=True, blank=True)
     name1=models.CharField(max_length=30, null=True, blank = True)
     repo1=models.FileField(upload_to='documents/',null=True, blank=True)
     name2=models.CharField(max_length=30, null=True, blank = True)
     repo2=models.FileField(upload_to='documents/',null=True, blank=True)
-    name3=models.CharField(max_length=30, null=True, blank = True)
-    repo3=models.FileField(upload_to='documents/',null=True, blank=True)
 
